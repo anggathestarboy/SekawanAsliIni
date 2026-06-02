9e1d824dcff19417710f980b0ed4bf07bcdd98d2
anggathestarboy <anggarathestarboy@gmail.com>
2026-06-02 09:19:54 +0700

    yang satu bener asli ini

---
+++ database/factories/ProductModelFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Model;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Model>
+ */
+class ProductModelFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "product_name" => fake() -> name(),
+            "product_price" => 2000,
+            "product_stock" => 23
+        ];
+    }
+}
