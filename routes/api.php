52dd15e4bd34214a04e54d1a91bbc7245fc5f376
anggathestarboy <anggarathestarboy@gmail.com>
2026-06-02 11:13:46 +0700

    modul4 done

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductCategoriesController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
@@ -11,5 +12,10 @@ Route::get('/user', function (Request $request) {
 
 
 
-Route::apiResource('/products', ProductController::class);
-Route::apiResource('/product-categories', ProductCategoriesController::class);
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::apiResource('/products', ProductController::class);
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/product-categories', ProductCategoriesController::class);
+});
